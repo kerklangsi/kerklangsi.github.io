80204d32220e9392e76959d39fc3f0ee9f4f2097
kerklangsi <arifilah.01@gmail.com>
2025-05-30 04:39:21 +0800

    Update location.js

--- location.js
+++ location.js
@@ -16,9 +16,18 @@ function updateLocationCard() {
       <h3>GPS Location</h3>
       <p>Lat: ${latitude}</p>
       <p>Lng: ${longitude}</p>
+      <iframe
+        width="100%"
+        height="300"
+        frameborder="0"
+        style="border:0; margin-top: 10px;"
+        src="https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&output=embed"
+        allowfullscreen>
+      </iframe>
     `;
   }
 }
+
 
 onValue(latRef, (snapshot) => {
   const val = snapshot.val();
